44a9c78b5c7d05f5a4cd1f3ef4bcf0eede176bc0
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-09-13 11:26:37 -0600

    Create ComputeArea.java

---
+++ ComputeArea.java
@@ -0,0 +1,14 @@
+public class ComputeArea {
+  public static void main(String[] args) {
+    double radius;
+    double area;
+
+    // Step 1: Read the radius
+    radius = 20;
+    
+    // Step 2: Compute area
+    area = radius * radius * 3.14159;
+    // Step 3: Display the area
+    System.out.println("The area for the circle of radius " + radius + " is " + area);
+  }
+}
