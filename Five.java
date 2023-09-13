a5005be18294991e4effa570100b4d018d3964e2
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-09-13 10:56:18 -0600

    Create Five.java

---
+++ Five.java
@@ -0,0 +1,8 @@
+public class Five {
+    public static void main(String[] args) {
+        String five = "Welcome to Java!";
+        for (int i = 0; i < 5; i++) {
+            System.out.println(five);
+        }
+    }
+}
