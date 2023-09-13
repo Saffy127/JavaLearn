69019d81f0adbdfee9efc840bf0ded11fb942876
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-09-12 19:31:25 -0600

    Create ComputeExpression.java

---
+++ ComputeExpression.java
@@ -0,0 +1,8 @@
+public class ComputeExpression {
+  public static void main(String[] args) {
+    System.out.println("(10.5 + 2 * 3) / (45 - 3.5) = ");
+    System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    System.out.println("3.5 * 4 / 2 – 2.5 is ");
+    System.out.println(3.5 * 4 / 2 – 2.5);
+  }
+}
