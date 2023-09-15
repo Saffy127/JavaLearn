2b3a378c9b3332887a2b0cd5510cab086eda83b8
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-09-14 18:24:10 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+class HelloWorld {
+  public static void main(String[] args) {
+    // This a comment in Java
+    int age;
+    age = 22;
+
+    System.out.println("I am " + age + " years old.");
+  }
 }
